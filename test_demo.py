c3154166c7ee84745bcf0004c78e9cc781025389
russq <russellq2003@sina.com>
2023-08-18 17:12:43 +0800

    test

---
+++ test_demo.py
@@ -0,0 +1,20 @@
+class TestDemo:
+    def test_one(self):
+        assert True
+    def test_two(self):
+        assert True
+    def test_three(self):
+        assert True
+
+def add(a, b):
+    """
+    This function adds two numbers.
+
+    Parameters:
+    a (int): The first number.
+    b (int): The second number.
+
+    Returns:
+    int: The sum of a and b.
+    """
+    return a + b
